64068bbb125a91c740783958d492df298fba4805
slavanorm <512f@bk.ru>
2025-04-27 02:24:06 +0400

    next

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
-GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
-GOOGLE_REDIRECT_URI = os.getenv('GOOGLE_REDIRECT_URI')
-GOOGLE_SCOPES= os.getenv('GOOGLE_SCOPES').split(',')
